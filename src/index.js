9541b378b6dff9a2c44ac14a5adc458633b9ca20
Natalia Górska <nataliagorska27@gmail.com>
2021-11-26 11:22:21 +0100

    adding modal

--- src/index.js
+++ src/index.js
@@ -3,11 +3,28 @@ import './style.css';
 
 const wheelContainer = document.querySelector(".wheelparts-container");
 const spinButton = document.getElementById("spin-button");
+const modal = document.getElementById("modal");
+const closeModalButton = document.querySelector(".close-button");
 
-const degreesOfRotate = ['320', '655', '675', '1020', '1050', '1400', '1410', '1405', '1750'];
+const degreesOfRotate = ['655', '675', '1020', '1050', '1400', '1410', '1405', '1750', '1755'];
 let randomDegrees = degreesOfRotate[Math.floor(Math.random() * degreesOfRotate.length)];
+
 
 spinButton.onclick = function () {
     wheelContainer.style.transform = "rotate(" + randomDegrees + "deg";
     randomDegrees = degreesOfRotate[Math.floor(Math.random() * degreesOfRotate.length)];
 }
+
+function closeModal () {
+    modal.classList.remove("modal__open");
+    modal.classList.add("modal__disable");
+}
+
+function openModal () {
+    modal.classList.remove("modal__disable");
+    modal.classList.add("modal__open");
+}
+
+closeModalButton.addEventListener('click', closeModal);
+
+wheelContainer.addEventListener('transitionend', openModal);
