aca1682a65cf1802a78f1cb399849031b7e64fb4
Natalia Górska <nataliagorska27@gmail.com>
2021-12-01 11:52:25 +0100

    improved js

--- src/index.js
+++ src/index.js
@@ -7,20 +7,34 @@ const modal = document.getElementById("modal");
 const modalWindow = document.querySelector(".modal-window");
 const closeModalSign = document.querySelector(".cross-sign");
 
+
 const degreesOfRotate = ['655', '675', '1020', '1050', '1400', '1410', '1405', '1750', '1755'];
 let randomDegrees = degreesOfRotate[Math.floor(Math.random() * degreesOfRotate.length)];
 
-
-spinButton.onclick = function () {
-    wheelContainer.style.transform = "rotate(" + randomDegrees + "deg";
+spinButton.addEventListener('click', function () {
+    spinButton.style.pointerEvents = "none";
+    wheelContainer.style.transform = "rotate(" + randomDegrees + "deg)";
     randomDegrees = degreesOfRotate[Math.floor(Math.random() * degreesOfRotate.length)];
-}
+    setTimeout(() =>  {
+        openModal();
+        party.confetti(spinButton, {
+            count: party.variation.range(40, 60),
+        });
+    }, 5200)
+});
+
+ function delayOfReturnWheel () {
+     setTimeout(() => {
+        return wheelContainer.style.transform = "none";
+     }, 1000)
+ }
 
 function closeModal () {
     modal.classList.remove("modal__open");
     modalWindow.classList.remove("open");
     modal.classList.add("modal__disable");
     modalWindow.classList.add("disabled");
+    spinButton.style.pointerEvents = "auto";
 }
 
 function openModal () {
@@ -28,9 +42,7 @@ function openModal () {
     modalWindow.classList.remove("disabled");
     modal.classList.add("modal__open");
     modalWindow.classList.add("open");
+    delayOfReturnWheel();
 }
 
-wheelContainer.addEventListener('transitionend', openModal);
-wheelContainer.addEventListener('transitionend', function (e) { party.confetti(this) } );
 closeModalSign.addEventListener('click', closeModal);
-
